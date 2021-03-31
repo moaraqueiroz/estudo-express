33474ae92ae270375931d4d990dae038a7a6cdcc
moara queiroz <moaraqueiroz@gmail.com>
2021-03-31 10:17:48 -0300

    usando o nodemon

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
-
 const app = express();
+// console.log(app);
 
-console.log(app);
+app.get('/', (req, res) => {
+    res.send('Olá');
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando!');
+})
